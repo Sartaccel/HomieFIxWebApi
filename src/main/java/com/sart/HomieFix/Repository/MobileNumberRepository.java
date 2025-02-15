32c461b7a2f7d320f91b3fa391040a98a7d218c0
Jeba Suwin <148338299+jebasuwin@users.noreply.github.com>
2025-02-15 17:15:37 +0530

    Delete src directory

--- src/main/java/com/sart/HomieFix/Repository/MobileNumberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sart.HomieFix.Repository;
-
-import com.sart.HomieFix.Entity.MobileNumber;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MobileNumberRepository extends JpaRepository<MobileNumber, Long> {
-    MobileNumber findByMobileNumber(String mobileNumber);
-}
--- src/main/java/com/sart/HomieFix/Repository/UserProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sart.HomieFix.Repository;
-
-import com.sart.HomieFix.Entity.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
-    UserProfile findByMobileNumberId(Long mobileId);
-}
