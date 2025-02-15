79f7df2bf39dff16725b910f40a20fabc6ddc973
Jeba Suwin <148338299+jebasuwin@users.noreply.github.com>
2025-02-15 17:18:57 +0530

    commit

--- src/main/java/com/sart/HomieFix/Repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sart.HomieFix.Repository;
-
-import com.sart.HomieFix.Entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
--- src/main/java/com/sart/HomieFix/Repository/WorkerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sart.HomieFix.Repository;
-
-import com.sart.HomieFix.Entity.Worker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
-}
