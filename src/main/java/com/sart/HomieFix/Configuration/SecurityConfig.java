ec23e07da109044fda68c26fa8d75835d3aa22a5
Jeba Suwin <148338299+jebasuwin@users.noreply.github.com>
2025-02-27 17:06:01 +0530

    commit

--- src/main/java/com/sart/HomieFix/Configuration/SecurityConfig.java
+++ src/main/java/com/sart/HomieFix/Configuration/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Enable CORS
                 .csrf(csrf -> csrf.disable()) // Disable CSRF protection for stateless authentication
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/products/**","/booking/**", "/profile/**", "/address/**", "/mobile/**", "/workers/**").permitAll() // Allow signup and login without authentication
+                        .requestMatchers("/auth/**", "/products/**","/booking/**", "/profile/**", "/address/**", "/mobile/**", "/workers/**","/feedback/**" , "/cart/**").permitAll() // Allow signup and login without authentication
                         .anyRequest().authenticated() // Protect all other endpoints
                 )
                 .sessionManagement(session -> session
